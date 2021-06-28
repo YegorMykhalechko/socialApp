f3cb5cce87ef3ee0cda9799d4964936a7f9faf18
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-06-28 19:20:27 +0200

    use effect friends

--- src/components/FriendsMain/FriendsMain.js
+++ src/components/FriendsMain/FriendsMain.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import * as axios from 'axios';
 
 import './FriendsMain.css';
 
 const FriendsMain = (props) => {
 
-
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").
-        then(res=>{
-            props.setUser(res.data.items)
-        })
-    }
+    useEffect (() => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").
+                then(res => {
+                    props.setUser(res.data.items)
+                })
+        }
+    });
 
     return <div className="friends">
         {
@@ -19,7 +20,7 @@ const FriendsMain = (props) => {
                 <div className="friends__wrapper">
                     <div className="friends__main">
                         <div>
-                            <img className="friends__avatar" src={u.photos.small != null ? u.photos.small: "https://image.freepik.com/darmowe-wektory/profil-czlowieka-avatar-na-rundy-ikona_24640-14044.jpg"}/>
+                            <img className="friends__avatar" src={u.photos.small != null ? u.photos.small : "https://image.freepik.com/darmowe-wektory/profil-czlowieka-avatar-na-rundy-ikona_24640-14044.jpg"} />
                         </div>
                         <div className="friends__btn">
                             {u.followed ? <button onClick={() => { props.follow(u.id) }}>Follow</button> : <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>}
